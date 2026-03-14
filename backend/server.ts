4896b8b2faff8e24cfafef1780c907f49983f6fc
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-03-13 22:27:11 -0400

    working onthis typescript project

---
+++ backend/server.ts
@@ -0,0 +1,11 @@
+import 'dotenv/config'; 
+import app from './app.js';  
+
+//database connection can go here as well. 
+
+const PORT = process.env.PORT;  
+
+app.listen(PORT, () => { 
+    console.log(`Server Successfully running on http://localhost:${PORT}`);
+}); 
+
