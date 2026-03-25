8faf4687a680fe7e4a74d4bb07ec9f552ca2d6a1
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-03-25 14:34:12 -0400

    working on the backend

--- backend/controllers/clientPref.controller.ts
+++ backend/controllers/clientPref.controller.ts
@@ -2,10 +2,20 @@ import { Request, Response } from "express";
 // import database as well 
 
 // import external service function here as such -> define the service function. 
+import {externalapifunction}  from '../services/externalapifunction.service.js'
 
 const clientpropdataController = async (req: Request, res: Response) => {
   // get user address data here as such 
+  try { 
+    const address = req.body;  
+    if (!address || address.length === 0) { 
+      
+    }
 
+  } catch (error) { 
+
+  }
+  // call the user function here as such 
 };
 
 export default clientpropdataController;
---
+++ backend/services/externalapifunction.service.ts
@@ -0,0 +1,27 @@
+interface clientAddress { 
+    UserAddress: string 
+};  
+
+
+const externalapifunction = async (Address : clientAddress) => { 
+    // handle cases here create try and catch case as such 
+    try { 
+        if (!Address || Address.UserAddress.length === 0) { 
+            throw new Error('Error no address entered');   
+        } 
+        console.log('Address entered');  
+        // build the external function here as such 
+        const buildingURL = new URLSearchParams({ 
+            address: Address.UserAddress
+        });  
+
+        const externalapiURL = `https://api.rentcast.io/v1/properties?${buildingURL}` 
+        console.log(externalapiURL); 
+        return; 
+    } catch (error) { 
+        console.error('Error', error);  
+        return; 
+    }
+}; 
+
+export default { externalapifunction }; 
