dba1bcfc9331b8e99f38fc3871e12132f84434bd
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-04-05 13:40:42 -0400

    analysis controller and route setup right now

---
+++ backend/controllers/propertyanalysis.controller.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express'; 
+import OpenAi from 'openai'; 
+
+// add openai api key to the .env folder in the backend 
+const openai = new OpenAi({ 
+    apiKey: process.env.OPENAIKEY
+});  
+
+interface clientAddress { 
+    Useraddress: string
+}; 
+
+const PropertyAnalysisController = (Address : clientAddress, req: Request, res: Response) => { 
+    res.status(200).json({ 
+        Message: "Controller is set up for analysis need openai api set up and call external function here"
+    }); 
+}
+
+export default PropertyAnalysisController; 
